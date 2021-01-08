96acdec81e164bb966b15c7c8da34b1f9c7e0f9e
MENA Coders <7216926+menacoders@users.noreply.github.com>
2021-01-08 23:32:33 +0200

    Add files via upload

---
+++ Menacoders/Conf/Plugin/Product/Type/Configurable/Price.php
@@ -0,0 +1,15 @@
+<?php
+namespace Menacoders\Conf\Plugin\Product\Type\Configurable;
+class Price
+{
+    public function aroundGetPrice($subject, $proceed,$product)
+    {
+       if($product->getTypeId()=="configurable"){
+           return $product->getFinalPrice();
+       }
+       else {
+           $returnValue = $proceed($product);
+           return $returnValue;
+       }
+    }
+}
